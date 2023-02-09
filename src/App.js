2d986b1e1efbea6929ffd617bba2a57f697c209e
Jakob Krigovsky <jakob@krigovsky.com>
2023-02-09 16:48:34 +0100

    Add auxiliary information

--- src/App.js
+++ src/App.js
@@ -1,37 +1,21 @@
-import React, {useEffect, useState} from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
-
-const GITHUB_ENDPOINT = 'https://api.github.com/repos/facebook/react/releases';
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import List from './List';
 
 function App() {
-  const [releases, setReleases] = useState([]);
-
-  useEffect(() => {
-    async function getResponse() {
-      const cache = await caches.open('github');
-      const match = await cache.match(GITHUB_ENDPOINT);
-
-      if (match) {
-        return match;
-      }
-
-      await cache.add(GITHUB_ENDPOINT);
-      return cache.match(GITHUB_ENDPOINT);
-    }
-
-    getResponse(GITHUB_ENDPOINT)
-      .then(response => response.json())
-      .then(result => setReleases(result));
-  }, []);
-
   return (
-    <ListGroup>
-      {releases.map(release => (
-        <ListGroup.Item key={release.id}>
-          {release.tag_name} on {release.published_at}
-        </ListGroup.Item>
-      ))}
-    </ListGroup>
+    <Container>
+      <Row>
+        <Col>
+          <h1>React Releases</h1>
+          <p>All releases of <a href='https://github.com/facebook/react/'>facebook/react</a> on GitHub.</p>
+
+          <List/>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
---
+++ src/List.js
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+const GITHUB_ENDPOINT = 'https://api.github.com/repos/facebook/react/releases';
+
+function List() {
+  const [releases, setReleases] = useState([]);
+
+  useEffect(() => {
+    async function getResponse() {
+      const cache = await caches.open('github');
+      const match = await cache.match(GITHUB_ENDPOINT);
+
+      if (match) {
+        return match;
+      }
+
+      await cache.add(GITHUB_ENDPOINT);
+      return cache.match(GITHUB_ENDPOINT);
+    }
+
+    getResponse(GITHUB_ENDPOINT)
+      .then(response => response.json())
+      .then(result => setReleases(result));
+  }, []);
+
+  return (
+    <ListGroup>
+      {releases.map(release => (
+        <ListGroup.Item key={release.id}>
+          {release.tag_name} on {release.published_at}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
+}
+
+export default List;

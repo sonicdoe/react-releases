455997489fc1d96208f2d35e0c4decd739fba8e3
Jakob Krigovsky <jakob@krigovsky.com>
2023-02-09 16:36:37 +0100

    Add mock list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
+import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+
 function App() {
+  return (
+    <ListGroup>
+      <ListGroup.Item>
+        v18.2.0 on 2022-06-14
+      </ListGroup.Item>
+    </ListGroup>
+  );
 }
 
 export default App;

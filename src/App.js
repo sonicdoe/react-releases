76dbed9540d131c318e37a82d7abd83e6e8b86c1
Jakob Krigovsky <jakob@krigovsky.com>
2023-02-09 16:40:49 +0100

    Fetch actual data from GitHub

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 
+const GITHUB_ENDPOINT = 'https://api.github.com/repos/facebook/react/releases';
+
 function App() {
+  const [releases, setReleases] = useState([]);
+
+  useEffect(() => {
+    fetch(GITHUB_ENDPOINT)
+      .then(response => response.json())
+      .then(result => setReleases(result));
+  }, []);
+
   return (
     <ListGroup>
-      <ListGroup.Item>
-        v18.2.0 on 2022-06-14
-      </ListGroup.Item>
+      {releases.map(release => (
+        <ListGroup.Item key={release.id}>
+          {release.tag_name} on {release.published_at}
+        </ListGroup.Item>
+      ))}
     </ListGroup>
   );
 }

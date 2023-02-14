5a339d4fd81cb043d24c0499b02fcd4d9d2033c6
Jakob Krigovsky <jakob@krigovsky.com>
2023-02-14 17:50:57 +0100

    Add Vim-style keyboard navigation

--- src/List.js
+++ src/List.js
@@ -2,12 +2,14 @@ import React, {useEffect, useState} from 'react';
 import Form from 'react-bootstrap/Form';
 import ListGroup from 'react-bootstrap/ListGroup';
 import {DateTime} from 'luxon';
+import {useHotkeys} from 'react-hotkeys-hook';
 
 const ENDPOINT = '/api/releases';
 
 function List() {
   const [releases, setReleases] = useState([]);
   const [query, setQuery] = useState('');
+  const [cursor, setCursor] = useState('');
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -30,6 +32,55 @@ function List() {
     };
   }, [query]);
 
+  useEffect(() => {
+    const cursorRelease = releases.find(release => release.id === cursor);
+
+    // Clear cursor if release no longer exists.
+    if (!cursorRelease) {
+      setCursor('');
+    }
+  }, [releases, cursor]);
+
+  useHotkeys(['j', 'k'], event => {
+    const direction = event.key === 'j' ? 'down' : 'up';
+
+    if (!releases || releases.length === 0) {
+      return;
+    }
+
+    if (!cursor) {
+      if (direction === 'down') {
+        setCursor(releases.at(0).id);
+      } else {
+        setCursor(releases.at(-1).id);
+      }
+
+      return;
+    }
+
+    const cursorReleaseIndex = releases.findIndex(release => release.id === cursor);
+    const adjacentRelease = direction === 'down'
+      ? releases[cursorReleaseIndex + 1]
+      : releases[cursorReleaseIndex - 1];
+
+    if (!adjacentRelease) {
+      return;
+    }
+
+    setCursor(adjacentRelease.id);
+  });
+
+  useHotkeys('enter', () => {
+    if (!cursor) {
+      return;
+    }
+
+    const cursorRelease = releases.find(release => release.id === cursor);
+    const url = cursorRelease.html_url;
+
+    window.location = url;
+  });
+
   const onQueryChange = event => {
     setQuery(event.target.value);
   };
@@ -46,10 +97,11 @@ function List() {
 
       <ListGroup variant='flush'>
         {releases.map(release => {
+          const currentlySelected = cursor === release.id;
           const publishedAt = DateTime.fromISO(release.published_at);
 
           return (
-            <ListGroup.Item key={release.id} action href={release.html_url}>
+            <ListGroup.Item key={release.id} action href={release.html_url} variant={currentlySelected && 'secondary'}>
               <span className='font-monospace'>{release.tag_name}</span> on {publishedAt.toLocaleString()}
             </ListGroup.Item>
           );

466cf661d18166f55927da655f1d8fcb6bb3b3c7
Jakob Krigovsky <jakob@krigovsky.com>
2023-02-14 16:50:16 +0100

    Add support for pagination

--- api/proxy.js
+++ api/proxy.js
@@ -10,20 +10,30 @@ const GITHUB_REPO = 'react';
 const octokit = new Octokit();
 
 export default async () => {
-  const response = await octokit.rest.repos.listReleases({
+  // The paginate function directly returns an array of data objects, so we use
+  // the map function to extract the last response’s headers into this variable.
+  let headers;
+
+  const releases = await octokit.paginate(octokit.rest.repos.listReleases, {
     owner: GITHUB_OWNER,
     repo: GITHUB_REPO,
+    // eslint-disable-next-line camelcase
+    per_page: 100,
+  }, response => {
+    headers = response.headers;
+
+    return response.data;
   });
 
-  return new Response(JSON.stringify(response.data), {
+  return new Response(JSON.stringify(releases), {
     headers: {
       // Cache for a week to avoid running into GitHub’s rate limit.
       'Cache-Control': 'public, max-age=604800',
       'Content-Type': 'application/json',
-      'X-RateLimit-Limit': response.headers['X-RateLimit-Limit'],
-      'X-RateLimit-Remaining': response.headers['X-RateLimit-Remaining'],
-      'X-RateLimit-Reset': response.headers['X-RateLimit-Reset'],
-      'X-RateLimit-Used': response.headers['X-RateLimit-Used'],
+      'X-RateLimit-Limit': headers['x-ratelimit-limit'],
+      'X-RateLimit-Remaining': headers['x-ratelimit-remaining'],
+      'X-RateLimit-Reset': headers['x-ratelimit-reset'],
+      'X-RateLimit-Used': headers['x-ratelimit-used'],
     },
   });
 };
